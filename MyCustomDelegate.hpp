643c339257384705086be65746a9cbd09eb25775
francisco melazzini <francisco.melazzini@physics.msu.ru>
2022-07-27 11:10:38 +0300

    started mvp and some code with qpalette

---
+++ MyCustomDelegate.hpp
@@ -0,0 +1,14 @@
+#ifndef _MYCUSTOMDELEGATE_HPP_
+#define _MYCUSTOMDELEGATE_HPP_
+
+#include<QStyledItemDelegate>
+
+struct MyCustomDelegate: QStyledItemDelegate
+{
+
+    
+
+};
+
+
+#endif //_MYCUSTOMDELEGATE_HPP_
